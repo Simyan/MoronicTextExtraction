4518ef2ee1e25ee866334c1a818900ed038ade24
Simyan <simyan10@gmail.com>
2019-11-25 00:16:40 +0400

    Worked on extraction logic + tested with csv file output

--- MoronicTextExtraction.py
+++ MoronicTextExtraction.py
@@ -5,18 +5,85 @@ import csv
 
 smsText = "Purchase of AED 6.19 with Debit Card ending 2932 at W MART SUPERMARKET MARI, DUBAI. Avl Balance is AED 1,244.68."
 
-filename = "ENBDMessages.txt"
+filename = "C:\\pyy\\ENBDMessages.txt"
 
+class Transactions:
+    def __init__(self, amount, location, timestamp, balance, transactionType):
+        self.amount = amount
+        self.location = location
+        self.timestamp = timestamp
+        self.balance = balance
+        self.transactionType = transactionType
+    
 def tokenIt(text):
-    #tokenz = nltk.word_tokenize(text)
-    tokenz = text.split()
-    return tokenz
+    #tokens = nltk.word_tokenize(text)
+    tokens = text.split()
+    return tokens
+
+def prepareTransactionIdentifiers():
+    ListOfWords = []
+    #PSDCwRC
+    ListOfWordsForPurchase = ["Purchase", "Avl", "Balance", "AED", "Debit", "Card"]
+    ListOfWordsForSalary = ["Salary", "credited", "account"]
+    ListOfWordsForDebit = ["has", "been", "debited", "account" ]
+    ListOfWordsForWithdrawal = ["Cash", "Withdrawal", "Debit", "Card"]
+    ListOfWordsForRefund = ["Purchase", "Debit", "Card", "refunded"]
+    ListOfWordsForCredit = ["credited", "Current", "balance"]
+
+    ListOfWords.append(ListOfWordsForPurchase)
+    ListOfWords.append(ListOfWordsForSalary)
+    ListOfWords.append(ListOfWordsForDebit)
+    ListOfWords.append(ListOfWordsForWithdrawal)
+    ListOfWords.append(ListOfWordsForRefund)
+    ListOfWords.append(ListOfWordsForCredit)
+
+    return ListOfWords
 
 
-def isTransaction(tokenz):
-    ListOfWords = ["Purchase", "Avl", "Balance", "AED", "Debit", "Card"]
-    result = all(elem in tokenz for elem in ListOfWords)
-    return result
+def isTransaction(tokens):
+    ListOfWords = prepareTransactionIdentifiers()    
+    listIndex = 0
+    for SubList in ListOfWords:        
+        result = all(elem in tokens for elem in SubList)
+        #x = listIndex if result else (listIndex += 1)
+        if result: 
+            return listIndex
+        else: 
+            listIndex += 1
+    return -1
+
+
+def extractofy(tokens, listIndex):
+    #if purchase extract words
+    #if salary extract 
+    #PSDCwRC
+    features = [[["at", "Avl"], 6, -4, -3, "Purchase"], ["ADCB", 6, -4, -3, "Salary"], ["ENBD", 4, -4, -3, "Debit"], ["Withdraw", 7, -4, -3, "Withraw"], [["at", "on"], 7, -4, -3, "Refund"], ["Deposit", 4, 15, -3, "Credit"]]
+    featuresOf = features[listIndex]
+    location = getValue(tokens, featuresOf[0] )
+    amount = getValue(tokens, featuresOf[1])
+    balance = getValue(tokens, featuresOf[2])
+    #balance = balance.replace(".,", "")
+    timestamp = getValue(tokens, featuresOf[3])
+    #timestamp = timestamp.replace(",", "")
+    #TODO: Write get type function
+    transactionType = featuresOf[4]
+    #transactionType = "DUMMY VALUE"
+    transaction = Transactions(amount, location, timestamp, balance, transactionType)
+    return transaction
+
+def getValue(tokens, features):
+    if isinstance(features, list):
+        start = tokens.index(features[0]) + 1
+        end = tokens.index(features[1])
+        loc = tokens[start:end]
+        return " ".join(loc)
+    elif isinstance(features, str):
+        return features
+    val = tokens[features]
+    val = val.replace(".,", "")
+    val = val.replace(",", "")
+    return val 
+
 
 def getLocation(tokens):
     isItOverYet = True
@@ -40,25 +107,31 @@ def writeToCSV(locations):
         for loc in locations:
             writer.writerows(loc)
 
-def writeToCSV2(locations):
-    csvFile = "output.csv"
+def writeToCSV2(rows):
+    csvFile = "outputTransactions.csv"
     with open(csvFile,'w') as result_file:
         wr = csv.writer(result_file, dialect='excel')
-        wr.writerow(locations)
+        transactions = []
+        for row in rows:
+            t = "Amount: " + row.amount + " | Location: " + row.location + " | Timestamp: " + row.timestamp + " | Balance: " + row.balance + " | Type: " + row.transactionType
+            transactions.append(t)
+        wr.writerow(transactions)
 
-
+#Main flow
 f = open(filename, "r", encoding="utf8")
 transactionCount = 0
 Locations = []
-for x in f:
+TransactionList = []
+for row in f:
   #print(x)
-  tokenz = tokenIt(x)
-  if isTransaction(tokenz):
-      transactionCount = transactionCount + 1
-      Locations.append(getLocation(tokenz))
-      
-      print (transactionCount)
-writeToCSV2(Locations)   
+  tokens = tokenIt(row)
+  listIndex = isTransaction(tokens)
+  if listIndex > -1:
+      TransactionList.append(extractofy(tokens, listIndex))
+      #transactionCount = transactionCount + 1
+      #Locations.append(getLocation(tokens))
+      #print (transactionCount)
+writeToCSV2(TransactionList)   
 
 
 
